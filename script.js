afa5d247a819c3bfb4253bb7753fe03d58035ed8
Pugsby <pugsbypuggerson@duck.com>
2025-04-20 19:12:57 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const inputElement = document.getElementById('input');
 const outputElement = document.getElementById('output');
-var commandList = ['print', 'help', 'installedLibraries', 'execute', 'credits'];
+var commandList = ['print', 'help', 'enabledLibraries', 'installedLibraries', 'enableLibrary', 'execute', 'credits', 'installLibrary'];
+var enabledLibraries = [];
 var installedLibraries = [];
 var allowACE = false;
 
@@ -28,13 +29,63 @@ document.addEventListener('keydown', (event) => {
             case 'help':
                 log('Available commands: ' + commandList.join(', '));
                 break;
+            case 'enabledLibraries':
+                if (enabledLibraries.length > 0) {
+                    log('Enabled Libraries: ' + enabledLibraries.join(', '));
+                } else {
+                    log('No libraries enabled.', "yellow");
+                }
+                break;
             case 'installedLibraries':
                 if (installedLibraries.length > 0) {
-                    log('Installed libraries: ' + installedLibraries.join(', '));
+                    log('Installed Libraries: ' + installedLibraries.join(', '));
                 } else {
                     log('No libraries installed.', "yellow");
                 }
+            case 'enableLibrary':
+                if (words[1]) {
+                    if (installedLibraries.includes(words[1])) {
+                        if (!enabledLibraries.includes(words[1])) {
+                            enabledLibraries.push(words[1]);
+                            commandList.push(...eval("myCommandList_" + words[1]));
+                            log(`Library ${words[1]} enabled.`);
+                        } else {
+                            log(`Library ${words[1]} is already enabled.`, "yellow");
+                        }
+                    } else {
+                        log(`Library ${words[1]} is not installed.`, "red");
+                    }
+                } else {
+                    log('Please specify a library to enable.', "yellow");
+                }
                 break;
+            case 'installLibrary':
+                if (words[1]) {
+                    fetch(words[1])
+                        .then(response => {
+                            if (!response.ok) {
+                                throw new Error(`HTTP error! status: ${response.status}`);
+                            }
+                            return response.text();
+                        })
+                        .then(scriptContent => {
+                            try {
+                                eval(scriptContent);
+                                installedLibraries.push(words[1]);
+                                log(`Library from ${words[1]} installed successfully.`);
+                                if (eval("ver_" + words[1]) != 2) {
+                                    log(`${words[1]} version ${eval("ver_" + words[1])} does not match current pgl version (2).`, "yellow");
+                                }
+                            } catch (error) {
+                                log(`Error executing library script: ${error.message}`, "red");
+                            }
+                        })
+                        .catch(error => {
+                            log(`Error fetching library: ${error.message}`, "red");
+                        });
+                } else {
+                    log('Please specify a URL to install the library from.', "yellow");
+                }
             case 'execute':
                 if (!allowACE) {
                     if (words[1] === 'true') {
@@ -64,19 +115,23 @@ document.addEventListener('keydown', (event) => {
                 log('Inspired by Windows Terminal and Linux Terminal.');
                 break;
             default:
-                if (handleCommandWithPdfReader) {
-                    var result = handleCommandWithPdfReader(words[0], words);
-                    if (result) {
+                var result = false;
+                enabledLibraries.forEach(library => {
+                    if (library) {
+                        try {
+                            result = eval("handleCommand_" + library + "('" + words[0] + "', '" + words + "')");
+                            if (result) {
+                                return;
+                            }
+                        } catch (error) {
+                            
+                        }
                         return;
                     }
+                });
+                if (!result) {
+                    log(`Unknown command: ${words[0]}`, "yellow");
                 }
-                if (handleCommandWithGames) {
-                    var result = handleCommandWithGames(words[0], words);
-                    if (result) {
-                        return;
-                    }
-                }
-                log(`Unknown command: ${words[0]}`, "yellow");
                 break;
             }
 
